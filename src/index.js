f81e63d5b6facaa98e30e53d1d99719fa43c3f5a
amwittlin <71662362+amwittlin@users.noreply.github.com>
2020-11-03 18:54:55 +0100

    added applicable icon

--- src/index.js
+++ src/index.js
@@ -26,23 +26,26 @@ function formatDate(timestamp) {
 
 
 function showTemperature(response){
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = "b032d609bc74d1e42dce6078fbbffc67";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Switzerland&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
